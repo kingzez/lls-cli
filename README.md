e82897f8a5c805f09edd2224061562f33d3f2139
kingzez <Vincent.Mr.W@gmail.com>
2022-10-14 00:56:32 +0800

    feat: add `lls` command

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # lls-cli
 
 A tool of list nodejs project node scripts and execute it.
+
+## Usage
+
+Execute `lls` in the project root directory, you will see node scripts list
+
+```bash
+lls
+```
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import fs from 'fs-extra'
+import { $ } from 'zx/core'
+import prompts from 'prompts'
+
+try {
+  const { scripts } = await fs.readJson('./package.json')
+  const choices = Object.keys(scripts).map((script) => ({
+    title: script,
+    value: script,
+  }))
+
+  const options = [
+    {
+      type: 'select',
+      name: 'command',
+      message: 'select node script',
+      choices,
+      initial: 0,
+    },
+  ]
+  // @ts-ignore
+  const { command } = await prompts(options)
+  if (command) {
+    // todo optimized
+    await $`npm run ${command}`
+  }
+} catch (err) {
+  console.error(err)
+}
